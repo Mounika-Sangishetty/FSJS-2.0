26b6e369136bf035949b56d35a6f46ca76fc229e
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-04-21 21:22:57 +0530

    file added

--- Practices/sample.js
+++ Practices/sample.js
@@ -1,1 +1,115 @@
-console.log('hello')
+//let firstname = "Mounika";
+//let lastname = "Sangishetty";
+//const mobileno = 9581525853;
+//console.log("My first name is",firstname);
+//console.log("My last name is",lastname);
+//console.log("My mobile number is "+mobileno);
+//console.log(firstname,lastname,mobileno);
+//console.log(`My current firstname is ${firstname}`);
+//console.log(`My Name is ${firstname} ${lastname} ${mobileno}`);
+//let x = 10;
+//let y = 10;
+//let z = x%y;
+//console.log(x*y);
+//let abc = 10;
+//let xyz = '10';
+//console.log(abc===xyz);
+
+//const balance = 1000;
+//balance = 2000;
+//console.log(balance);
+
+//let age = 25;
+//if(age>=18){
+ //   console.log('You are allowed inside the park');
+//} else {
+//   console.log('out of the park');
+//}
+    
+//let gender = 'male';
+//if (gender=='female'){
+ //   console.log('Yes you are allowed');
+//} else{
+//    console.log('You are not allowed');
+//}
+/*let standard = 1 ;
+if(standard==1){
+    console.log('Room 1');
+} else if(standard==2){
+console.log('Room 2');
+}else if(standard==3){
+    console.log('Room 3')
+}else{
+    console.log("You are Principal");
+}
+let user="Admin";
+switch(user){
+    case "Admin":
+        console.log("Redirect him/her to the Admin Page");
+        break;
+    case "Mentor":
+        console.log("Redirect him/her to the Mentor Dashborad");
+        break;
+    default:
+        console.log("I am simple user");
+}
+let raining = true;
+raining ? console.log ("It is Raining") : console.log("it is not Raining");
+
+let userlogin = true;
+userlogin ? console.log('logout') : console.log('login');
+
+let name = ["Mounika", "Pushpa", "Anusha", "Ramana", "Sowmya", "Geetha", "Latha"];
+console.log(name[5]);
+
+let bankaccount = {
+    firstname : "Mounika",
+    lastname : "Sangishetty",
+    mobileno : 9581525853,
+    balance : 1650,
+}
+console.log(bankaccount.balance);
+*/
+//const now = new Date();
+//console.log(now);
+//console.log(now.toString());
+//console.log(now.getFullYear());
+//console.log(now.getMonth());
+//console.log(now.getDay());
+//console.log(now.getTime());
+//console.log(now.getHours());
+//console.log(now.getMinutes());
+//console.log(now.getSeconds());
+
+const PI = Math.PI;
+//console.log(PI);
+//console.log(Math.round(PI));
+//console.log(Math.round(9.4));
+//console.log(Math.floor(9.5));
+//console.log(Math.ceil(9.5));
+//console.log(Math.min(10,5,6,15,20));
+//console.log(Math.max(20,15,13,25,16));
+//console.log(Math.random()*10);
+//console.log(Math.round(Math.random()*51));
+//console.log(Math.pow(5,4));
+//console.log(Math.log(2));
+//console.log(Math.tan(90));
+/*let i=0;
+do{
+    console.log("value of i is:",i);
+    i++;
+}while(i<=5);
+*/
+/*let j=0;
+while(j<=5){
+    console.log("value of j is",j);
+    j++
+}
+*/
+/*for (let i=0; i<=5; i++) {
+    console.log("value of i is",i);
+}*/
+let i=3;
+for(let j=0; j<=5; j++) {
+    console.log(`${i}*${j}=${i*j}`);
+}
