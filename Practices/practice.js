aef6148daedb593fc2736cf317b2de1f25acd44f
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-07-12 18:08:28 +0530

    file added

---
+++ Practices/practice.js
@@ -0,0 +1,88 @@
+let firstname = "Sangishetty";
+let lastname = "Mounika"
+//console.log(firstname, lastname);
+//console.log("My name is", firstname, lastname);
+//console.log(`My name is ${firstname} ${lastname}`);
+
+let x=20;
+let y=10;
+let z=x+y;
+//console.log(z);
+
+//console.log(x+y);
+//console.log(x-y);
+//console.log(x*y);
+//console.log(x/y);
+//console.log(x%y);
+
+//let abc=10;
+//let xyz=20;
+//console.log(abc>xyz);
+//console.log(abc<xyz);
+//console.log(abc>=xyz);
+//console.log(abc<=xyz);
+
+let abc=20;
+let xyz='20';
+//console.log(abc==xyz);
+//console.log(abc===xyz);
+
+//let age=16;
+//if(age==18){
+//    console.log("You are allowed inside the park");
+//}
+//console.log("Out of the park");
+
+//let age=16;
+//if(age>=18){
+//    console.log("You are allowed inside the park");
+//}else{
+//    console.log("Out of the park");
+//}
+
+//let gender="male";
+//if(gender=="female"){
+//    console.log("Yes, you are allowed");
+//}else{
+//    console.log("No, you are not allowed");
+//}
+
+//let standard=4;
+//if(standard==1){
+//    console.log("Room 1");
+//}else if(standard==2){
+//    console.log("Room 2");
+//}else if(standard==3){
+//    console.log("Room 3");
+//}else{
+//    console.log("You are Principal");
+//}
+
+//let rating=2;
+//if(rating==5){
+//    console.log("5 stars");
+//}else if(rating==4){
+//    console.log("4 stars");
+//}else if(rating==3){
+//    console.log("3 stars");
+//}else{
+//    console.log("i am else")
+//}
+
+//let user="user";
+//switch(user){
+//    case "Admin":
+//    console.log("Redirect him/her to the Admin Page");
+//    break;
+//    case "Mentor":
+//   console.log("Redirect him/her to the Mentor dashboard");
+//    break;
+//    default:
+//        console.log("I am a simple user")
+//}
+
+//let raining=true;
+//raining?console.log("It is raining"):console.log("It is not raining");
+
+//let userlogin=false;
+//userlogin?console.log("log out"):console.log("log in");
