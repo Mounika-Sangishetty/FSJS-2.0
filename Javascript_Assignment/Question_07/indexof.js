4c050c9d0e42551816e6422595fdb3e8386ea5c9
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 19:12:08 +0530

    file added

---
+++ Javascript_Assignment/Question_07/indexof.js
@@ -0,0 +1,9 @@
+// Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+let array = ["mounika", "manasa", "pw skills", "mallika", "anusha", "pushpa", "pw skills", "bindu", "lalitha", "radha", "pw skills", "alekya"];
+
+let array1 = array.lastIndexOf("pw skills");
+let array2 = array.lastIndexOf("pw skills", array1 - 1);
+
+console.log("The last occurance of a word (pw skills) is:",array1);
+console.log("The first occurance of a word (pw skills) is:",array2);
