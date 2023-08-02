c04b6f67c80c0b96e668d6e6e758e2a784f4e366
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 17:33:42 +0530

    file added

---
+++ Javascript_Assignment/Question_03/uppercase.js
@@ -0,0 +1,5 @@
+// Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+let myname = "sangishetty mounika";
+let capital = myname.toUpperCase();
+console.log(capital);
