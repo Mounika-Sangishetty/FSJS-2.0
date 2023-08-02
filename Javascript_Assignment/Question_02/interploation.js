115b9df798cacbeb542134c6dfe0fe1d3ca0e3c5
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 17:27:10 +0530

    file added

---
+++ Javascript_Assignment/Question_02/interploation.js
@@ -0,0 +1,13 @@
+// Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstname = "Mounika";
+let lastname = "Sangishetty";
+let maritalstatus = "married";
+let country = "India";
+let age = 26;
+
+console.log(`My First Name is: ${firstname}`);
+console.log(`My Last Name is: ${lastname}`);
+console.log(`My Marital Status is: ${maritalstatus}`);
+console.log(`My Country is: ${country}`);
+console.log(`My Age is: ${age}`);
