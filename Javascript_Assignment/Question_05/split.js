d6a6f0a56397b44817c7555e526aa1f4e3296de3
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 18:17:05 +0530

    file added

---
+++ Javascript_Assignment/Question_05/split.js
@@ -0,0 +1,7 @@
+//  Declare a varibale and assign string value to it and then split it into an array using split() method
+
+let text = "JavaScript is a high-level programming language.";
+
+let array = text.split(" ");
+
+console.log(array);
