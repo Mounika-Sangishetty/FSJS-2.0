2cec9968473d92ebdbb38d448efa6634cabaa4a7
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 19:53:20 +0530

    file added

---
+++ Javascript_Assignment/Question_10/expression.js
@@ -0,0 +1,43 @@
+// Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//    - 4 > 3
+//    - 4 >= 3
+//    - 4 < 3
+//    - 4 <= 3
+//    - 4 == 4
+//    - 4 === 4
+//    - 4 != 4
+//    - 4 !== 4
+//    - 4 != '4'
+//    - 4 == '4'
+//    - 4 === '4'
+//    - Find the length of python and jargon and make a falsy comparison statement.
+
+    - 4 > 3  //true
+    - 4 >= 3 //true
+    - 4 < 3  //false
+    - 4 <= 3 //false
+    - 4 == 4 //true
+    - 4 === 4 //true
+    - 4 != 4  //false
+    - 4 !== 4 //false
+    - 4 != '4' //false
+    - 4 == '4' //true
+    - 4 === '4' //false
+
+    console.log(4 > 3);
+    console.log(4 >= 3);
+    console.log(4 < 3);
+    console.log(4 <= 3);
+    console.log(4 == 4);
+    console.log(4 === 4);
+    console.log(4 != 4);
+    console.log(4 !== 4);
+    console.log(4 != '4');
+    console.log(4 == '4');
+    console.log(4 === '4');
+
+    let name1 = "python";
+    let name2 = "jargon";
+    console.log(name1.length);
+    console.log(name2.length);
+    console.log(name1.length != name2.length);
