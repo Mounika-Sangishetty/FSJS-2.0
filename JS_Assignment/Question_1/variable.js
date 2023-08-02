6c1db08250a15160f7a4a3523d242727eb02f9ff
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 16:43:34 +0530

    file added

---
+++ JS_Assignment/Question_1/variable.js
@@ -0,0 +1,8 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+let myname = "Mounika";
+const mynumber = 9581525853;
+let fbaccount = true;
+let twitteracc = false;
+
+console.log(myname)
