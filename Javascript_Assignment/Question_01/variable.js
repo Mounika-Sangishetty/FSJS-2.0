05f4f34e4f6804133ffddc4c139c7723ffc62628
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 17:18:19 +0530

    file added

---
+++ Javascript_Assignment/Question_01/variable.js
@@ -0,0 +1,18 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+let myname = "Mounika";
+const mynumber = 9581525853;
+let pi = 3.14;
+let fbaccount = true;
+var twitteracc = false;
+let mybalance;
+let signincount = null;
+
+
+console.log("myname is type of:", typeof(myname));
+console.log("mynumber is type of:", typeof(mynumber));
+console.log("pi is type of:", typeof(pi));
+console.log("fbaccount is type of:", typeof(fbaccount));
+console.log("twitteracc is type of:", typeof(twitteracc));
+console.log("mybalance is type of:", typeof(mybalance));
+console.log("signincount is type of:", typeof(signincount));
