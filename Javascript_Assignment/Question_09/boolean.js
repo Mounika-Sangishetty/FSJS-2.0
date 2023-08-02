b140bdacc480257ec83f40f262fc916276f507f9
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 19:37:41 +0530

    file added

---
+++ Javascript_Assignment/Question_09/boolean.js
@@ -0,0 +1,24 @@
+//  Boolean value is either true or false.
+//    - Write three JavaScript statement example which provide truthy value.
+//    - Write three JavaScript statement example which provide falsy value.
+
+// truthy values
+let statement1 = 10<20;
+console.log(statement1);
+
+let statement2 = 15 == "15";
+console.log(statement2);
+
+let array = [1,2,3,4,5];
+console.log(Array.isArray(array));
+
+// falsy values
+let statement3 = 15 === "15";
+console.log(statement3);
+
+let array1 = [1,2,3,4,5,6,"radha",8];
+console.log(array1.includes("radha",7));
+
+let myname = "mounika";
+console.log(Array.isArray(myname));
+
