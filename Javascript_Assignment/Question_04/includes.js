61eba3cd1159cc0f3fddb88ab8904474f11915af
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-02 18:02:55 +0530

    file added

---
+++ Javascript_Assignment/Question_04/includes.js
@@ -0,0 +1,6 @@
+// Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+let context = "JavaScript is a high-level, interpreted programming language primarily used for creating interactive and dynamic web content.";
+
+console.log("If the context contains the word script of (high-level):", context.includes("high-level"));
+console.log("If the context contains the word script of (HTML):", context.includes("HTML"));
