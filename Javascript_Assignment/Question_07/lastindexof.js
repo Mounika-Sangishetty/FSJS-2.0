2b712a45da8e7e8f3fe46db47e54868f1332876b
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-03 13:09:21 +0530

    file added

--- Javascript_Assignment/Question_07/lastindexof.js
+++ Javascript_Assignment/Question_07/lastindexof.js
@@ -3,7 +3,7 @@
 let array = ["mounika", "manasa", "pw skills", "mallika", "anusha", "pushpa", "pw skills", "bindu", "lalitha", "radha", "pw skills", "alekya"];
 
 let array1 = array.lastIndexOf("pw skills");
-let array2 = array.lastIndexOf("pw skills", array1 - 1);
+let array2 = array.lastIndexOf("pw skills", 0);
 
-console.log("The last occurance of a word (pw skills) is:",array1);
-console.log("The first occurance of a word (pw skills) is:",array2);
+console.log("The last occurance of a word (pw skills) is:", array1);
+console.log("The first occurance of a word (pw skills) is:", array2);
