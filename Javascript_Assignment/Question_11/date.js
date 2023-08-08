41459f98b291020d5b3e30160e16a9da23f37675
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-08 12:24:52 +0530

    file updated

--- Javascript_Assignment/Question_11/date.js
+++ Javascript_Assignment/Question_11/date.js
@@ -8,14 +8,12 @@
 //    - Find out the numbers of seconds elapsed from January 1, 1970 to now.
 
 const now = new Date();
-const date = new Date('January 1, 1970');
 console.log("The year of today is:", now.getFullYear());
-console.log("The year of today is:", date.getFullYear());
-console.log(date.getSeconds());
-console.log(now-date);
 console.log("The month of today as a number is:", now.getMonth());
 console.log("The date of today is:", now.getDate());
 console.log("The day of today as a number is:", now.getDay());
 console.log("The hours of now is:", now.getHours());
 console.log("The minutes of now is:", now.getMinutes());
-console.log("The number of seconds elapsed from January 1, 1970 to now is:", );
+let milliseconds = Date.now(); // the number of milliseconds since January 1, 1970
+let seconds = milliseconds / 1000;
+console.log("The number of seconds elapsed from January 1, 1970 to now is:", seconds);
