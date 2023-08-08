2ec63cfceb53f6656228e2b4e99ab67923602692
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-08 20:31:21 +0530

    file added

---
+++ DOM_Assignment/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/script.js
@@ -0,0 +1,7 @@
+// Task1
+// adding Hire me
+
+let hireme = document.createElement("li");
+hireme.innerHTML = "Hire me";
+//console.log(hireme);
+document.getElementById("navul").appendChild(hireme);
