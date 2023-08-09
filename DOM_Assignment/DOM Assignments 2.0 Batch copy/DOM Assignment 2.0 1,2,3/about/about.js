8fe99d93ea2d9505022a89efb08f59df5b1a9668
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-09 18:34:32 +0530

    file added

--- DOM_Assignment/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
+++ DOM_Assignment/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
@@ -9,3 +9,37 @@ accordian.forEach((element) => {
     }
   });
 });
+
+// second assignment
+// Task1
+// Adding background color to heading
+
+let heading = document.querySelectorAll(".accordian h3");
+heading.forEach(myfun);
+function myfun(h3) {
+    h3.style.backgroundColor = "#dadaf8";
+}
+
+// Task2
+// Adding a heading with text
+
+let heading4 = document.createElement("div");
+heading4.classList.add("accordian")
+heading4.id = "heading4";
+document.getElementById("headingsection").appendChild(heading4);
+let skills = document.createElement("h3");
+skills.innerText = "Skills";
+skills.style.backgroundColor = "#dadaf8";
+document.getElementById("heading4").appendChild(skills);
+let text = document.createElement("p");
+text.innerText = "I posses a very good command over the Full Stack Developmet technologies like MERN which can be seen in my work over the Github";
+document.getElementById("heading4").appendChild(text);
+
+skills.addEventListener("click", () =>{
+  let context = skills.nextElementSibling;
+  if (context.style.display === "block") {
+    context.style.display = "none";
+  } else {
+    context.style.display = "block";
+  }
+});
