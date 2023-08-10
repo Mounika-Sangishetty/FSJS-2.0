e842f07e5e600f279704d8f1e09154725564c0bc
Mounika-Sangishetty <mounikakarnati666@gmail.com>
2023-08-10 14:07:17 +0530

    file added

--- DOM_Assignment/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
+++ DOM_Assignment/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
@@ -1,6 +1,21 @@
 // Task
-// adding background color and color to text
+// adding background color 
 
 let card1 = document.querySelector(".clash-card__unit-stats--barbarian");
+card1.style.backgroundColor = "#ec9b3b";
+let card2 = document.querySelector(".clash-card__unit-stats--archer");
+card2.style.backgroundColor = "#ee5487";
+let card3 = document.querySelector(".clash-card__unit-stats--giant");
+card3.style.backgroundColor = "#f6901a";
+let card4 = document.querySelector(".clash-card__unit-stats--goblin");
+card4.style.backgroundColor = "#82bb30";
+let card5 = document.querySelector(".clash-card__unit-stats--wizard");
+card5.style.backgroundColor = "#4facff";
 
 
+// adding color to values
+
+let values = document.querySelectorAll(".one-third");
+values.forEach((text) => {
+    text.style.color = "#ffffff";
+})
